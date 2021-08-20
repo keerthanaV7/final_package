238a23f18373d62e192ed491f9979b389926f62a
keerthanaV7 <51898532+keerthanaV7@users.noreply.github.com>
2021-08-20 18:49:22 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+import os
+
+setuptools.setup(
+	name = "check3",
+	version="2.0",
+	author="Keerthana",
+	description=" final Package 3",
+	classifiers = [
+		"Programming Languages :: Python :: 3",
+		"Operating System :: OS Independent"],
+	python_requires ='>=3.6',
+	install_requires=['pandas==1.1.5']
+	
+)
